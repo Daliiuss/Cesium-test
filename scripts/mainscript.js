09b32bfc8456d328cb5bd4b7c561aef466714a92
Daliiuss <46731380+Daliiuss@users.noreply.github.com>
2019-07-13 20:03:41 +0300

    Added on map points

--- scripts/mainscript.js
+++ scripts/mainscript.js
@@ -5,6 +5,8 @@ var viewer = new Cesium.Viewer('cesiumContainer', {
     shouldAnimate : true
 });
 
+
+// Add model
 function createModel(url, height) {
     viewer.entities.removeAll();
 
@@ -29,11 +31,52 @@ function createModel(url, height) {
         viewer.trackedEntity = entity;
     }
 
+//Option bar
 var options = [
     {text : '---'},
     { text : 'Car',
       onselect : function() {createModel('../../SampleData/models/Car/Car.gltf', 0.0);}
     }
   ];
-
 Sandcastle.addToolbarMenu(options);
+
+var screenSpaceEventHandler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
+
+//Get position
+/*function getClickPosition(e) {
+    var xPosition = e.clientX;
+    var yPosition = e.clientY;
+}
+*/
+
+var points_array = [];
+    function fix_coordinate(clickPosition) {
+
+
+   }
+
+// Function to add a point at the mouse position
+   function addPoint(clickPosition) {
+       var point = viewer.entities.add({
+           position: clickPosition,
+           point: {
+               pixelSize: 10,
+               color: Cesium.Color.RED
+           }
+       });
+
+   }
+
+//get click position
+screenSpaceEventHandler.setInputAction(function(click) {
+// get position  of click
+    var clickPosition = viewer.camera.pickEllipsoid(click.position);
+    //print to console
+    console.log(clickPosition);
+    addPoint(clickPosition);
+    points_array += clickPosition;
+    console.log(points_array.length);
+
+
+},
+ Cesium.ScreenSpaceEventType.LEFT_CLICK);
